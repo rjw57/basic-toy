34d29e1974dc19f2e2900d6dcca5951ef69bc299
Rich Wareham <rich@richwareham.com>
2023-07-15 12:07:02 +0100

    add basic github actions configuration

--- tests/test_print_statement.py
+++ tests/test_print_statement.py
@@ -12,7 +12,7 @@ from rwbasic.interpreter import Interpreter
         ('PRINT "hello" : REM print a greeting', "hello\n"),
         ("PRINT 1+2*3+4", "11\n"),
         ('PRINT "hello, "+"world";', "hello, world"),
-    ]
+    ],
 )
 def test_expected_output(interpreter: Interpreter, line: str, expected_output: str, capsys):
     interpreter.execute(line)
