6d2128addb20b4daf5e60110f43ec1ab7e892898
Rich Wareham <rich@richwareham.com>
2023-07-16 08:23:56 +0100

    test loading program on command line

--- tests/test_run_module.py
+++ tests/test_run_module.py
@@ -15,3 +15,13 @@ def test_main(mocker):
     mocker.patch("sys.argv", ["rwbasic"])
     main()
     repl_mock.return_value.run.assert_called()
+
+
+def test_load_program(mocker, tmp_path, capsys):
+    test_program = tmp_path / "test.bas"
+    with open(test_program, "w") as f:
+        f.write("PRINT 2*5")
+    mocker.patch("sys.argv", ["rwbasic", str(test_program)])
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "10\n"
